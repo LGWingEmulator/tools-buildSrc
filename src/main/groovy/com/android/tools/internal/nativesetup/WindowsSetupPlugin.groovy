60035b6e4c3749c68877b0ebf34004b0aabfcc68
Jerome Dochez <jedo@google.com>
2014-06-24 15:16:48 -0700

    Added support for Gradle 2.0
    change is not backward compatible, will not compile or run on 1.1x
    
    Change-Id: I8ddb85b0f36c226306ef839cf1bae772f7c7cacd

--- src/main/groovy/com/android/tools/internal/nativesetup/WindowsSetupPlugin.groovy
+++ src/main/groovy/com/android/tools/internal/nativesetup/WindowsSetupPlugin.groovy
@@ -19,52 +19,12 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.nativebinaries.platform.Platform
 import org.gradle.nativebinaries.toolchain.Gcc
-import org.gradle.nativebinaries.toolchain.TargetPlatformConfiguration
 /**
  */
 class WindowsSetupPlugin implements Plugin<Project> {
 
-    class MingwOnLinuxConfiguration implements TargetPlatformConfiguration {
-
-        List<String> customLinkerArgs = ['-m32']
-
-        boolean supportsPlatform(Platform element) {
-            return element.getOperatingSystem().name == "windows"
-        }
-
-        List<String> getCppCompilerArgs() {
-            ['-DUSE_MINGW', '-D__STDC_FORMAT_MACROS', '-D__STDC_CONSTANT_MACROS', '-D__USE_MINGW_ANSI_STDIO', '-m32']
-        }
-
-        List<String> getCCompilerArgs() {
-            ['-DUSE_MINGW', '-D__STDC_FORMAT_MACROS', '-D__STDC_CONSTANT_MACROS', '-D__USE_MINGW_ANSI_STDIO', '-m32']
-        }
-
-        List<String> getObjectiveCCompilerArgs() {
-            []
-        }
-
-        List<String> getObjectiveCppCompilerArgs() {
-            []
-        }
-
-        List<String> getAssemblerArgs() {
-            []
-        }
-
-        List<String> getLinkerArgs() {
-            customLinkerArgs
-        }
-
-        List<String> getStaticLibraryArchiverArgs() {
-            []
-        }
-    }
-
     @Override
     void apply(Project project) {
-
-        project.ext.mingw = new MingwOnLinuxConfiguration()
 
         project.model {
             platforms {
@@ -78,13 +38,24 @@ class WindowsSetupPlugin implements Plugin<Project> {
                 mingw(Gcc) {
                     path "$project.rootDir/../prebuilts/gcc/linux-x86/host/x86_64-w64-mingw32-4.8/bin"
 
-                    addPlatformConfiguration(project.ext.mingw)
+                    target("windows") {
+                        cppCompiler.executable =  'x86_64-w64-mingw32-g++'
+                        cppCompiler.withArguments {
+                            '-DUSE_MINGW -D__STDC_FORMAT_MACROS -D__STDC_CONSTANT_MACROS -D__USE_MINGW_ANSI_STDIO -m32'
+                        }
 
-                    getCCompiler().executable =         'x86_64-w64-mingw32-gcc'
-                    getCppCompiler().executable =       'x86_64-w64-mingw32-g++'
-                    getLinker().executable =            'x86_64-w64-mingw32-g++'
-                    getAssembler().executable =         'x86_64-w64-mingw32-as'
-                    getStaticLibArchiver().executable = 'x86_64-w64-mingw32-ar'
+                        cCompiler.executable = 'x86_64-w64-mingw32-gcc'
+                        cCompiler.withArguments {
+                            '-DUSE_MINGW -D__STDC_FORMAT_MACROS -D__STDC_CONSTANT_MACROS -D__USE_MINGW_ANSI_STDIO -m32'
+                        }
+
+                        linker.executable = 'x86_64-w64-mingw32-g++'
+                        linker.withArguments {
+                            '-m32'
+                        }
+                        assembler.executable = 'x86_64-w64-mingw32-as'
+                        staticLibArchiver.executable = 'x86_64-w64-mingw32-ar'
+                    }
                 }
             }
         }
