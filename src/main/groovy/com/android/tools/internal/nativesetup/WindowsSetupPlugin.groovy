112e57913ab1cd0efe44235932d9e08b55f95552
Raymond Chiu <chiur@google.com>
2014-09-04 16:49:51 -0700

    Use gradle 2.2-20140904223451+0000
    Change-Id: I4dc59302481027181f8f2f705771b1f17aa86ea3

--- src/main/groovy/com/android/tools/internal/nativesetup/WindowsSetupPlugin.groovy
+++ src/main/groovy/com/android/tools/internal/nativesetup/WindowsSetupPlugin.groovy
@@ -17,8 +17,8 @@
 package com.android.tools.internal.nativesetup
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.nativebinaries.platform.Platform
-import org.gradle.nativebinaries.toolchain.Gcc
+import org.gradle.nativeplatform.platform.Platform
+import org.gradle.nativeplatform.toolchain.Gcc
 /**
  */
 class WindowsSetupPlugin implements Plugin<Project> {
