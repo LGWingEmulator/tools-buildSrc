dd19e315fddd5be76307b8066d66b94cb62e4cc5
Erwin Jansen <jansene@google.com>
2019-02-27 12:07:23 -0800

    Deprecate gradle build
    We will now call the build script directly. The build script will
    produce a distribution for us which contains everything of interest.
    
    Updated the logging to include timestamps.
    
    Change-Id: Ic2410f4a3ea36cf37f008fcd7656122214de1529

--- servers/build_tools.py
+++ servers/build_tools.py
@@ -19,121 +19,135 @@ from __future__ import print_function
 
 import logging
 import argparse
+import datetime
 import os
 import platform
 import sys
 import subprocess
+import time
 
 from Queue import Queue
 from threading import Thread
 
+AOSP_ROOT = os.path.abspath(
+    os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "..", ".."))
+TOOLS = os.path.join(AOSP_ROOT, "tools")
 
-AOSP_ROOT = os.path.abspath(os.path.join(
-    os.path.dirname(os.path.realpath(__file__)), '..', '..', '..'))
-TOOLS = os.path.join(AOSP_ROOT, 'tools')
 
-
+start_time = time.time()
 def _reader(pipe, queue):
-    try:
-        with pipe:
-            for line in iter(pipe.readline, b''):
-                queue.put((pipe, line[:-1]))
-    finally:
-        queue.put(None)
+  try:
+    with pipe:
+      for line in iter(pipe.readline, b""):
+        queue.put((pipe, line[:-1]))
+  finally:
+    queue.put(None)
+
+def log_line(prefix, line):
+  passed = datetime.timedelta(seconds=time.time() - start_time)
+  logging.info("%s %s| %s", passed, prefix, line.strip())
 
 
-def log_std_out(proc):
-    """Logs the output of the given process."""
-    q = Queue()
-    is_windows = (platform.system() == 'Windows')
-    Thread(target=_reader, args=[proc.stdout, q]).start()
-    Thread(target=_reader, args=[proc.stderr, q]).start()
-    for _ in range(2):
-        for _, line in iter(q.get, None):
-            logging.info(line.strip())
+
+def log_std_out(proc, log_prefix):
+  """Logs the output of the given process."""
+  q = Queue()
+  Thread(target=_reader, args=[proc.stdout, q]).start()
+  Thread(target=_reader, args=[proc.stderr, q]).start()
+  for _ in range(2):
+    for _, line in iter(q.get, None):
+        log_line(log_prefix, line)
 
 
-def run(cmd, env):
-    logging.info(' '.join(cmd))
-    cmd_env = os.environ.copy()
-    cmd_env.update(env)
-    is_windows = (platform.system() == 'Windows')
+def run(cmd, env, log_prefix):
+  log_line(log_prefix, "=" * 120)
+  log_line(log_prefix, " ".join(cmd))
+  cmd_env = os.environ.copy()
+  cmd_env.update(env)
+  is_windows = (platform.system() == "Windows")
 
-    proc = subprocess.Popen(
-        cmd,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        shell=is_windows, # Make sure windows propagates ENV vars properly.
-        cwd=AOSP_ROOT,
-        env=cmd_env)
+  proc = subprocess.Popen(
+      cmd,
+      stdout=subprocess.PIPE,
+      stderr=subprocess.PIPE,
+      shell=is_windows,  # Make sure windows propagates ENV vars properly.
+      cwd=AOSP_ROOT,
+      env=cmd_env)
 
-    log_std_out(proc)
-    proc.wait()
-    if proc.returncode != 0:
-        raise Exception('Failed to run %s - %s' %
-                        (' '.join(cmd), proc.returncode))
+  log_std_out(proc, log_prefix)
+  proc.wait()
+  if proc.returncode != 0:
+    raise Exception("Failed to run %s - %s" % (" ".join(cmd), proc.returncode))
 
 
 def install_deps():
-    run(['python', os.path.join(AOSP_ROOT, 'external', 'qemu', 'android',
-                                'build', 'python', 'setup.py'), 'develop', '--user'], {})
+  run([
+      "python",
+      os.path.join(AOSP_ROOT, "external", "qemu", "android", "build", "python",
+                   "setup.py"), "develop", "--user"
+  ], {}, "dep")
 
 
 def main(argv):
-    logging.basicConfig(format='%(message)s', level=logging.INFO)
-    parser = argparse.ArgumentParser(
-        description='Configures the android emulator cmake project so it can be build')
-    parser.add_argument("--out_dir", type=str, required=True,
-                        help="The ouput directory")
-    parser.add_argument("--dist_dir", type=str, required=True,
-                        help="The destination directory")
-    parser.add_argument("--build-id", type=str, default=[], required=True,
-                        dest='build_id',  help="The emulator build number")
-    parser.add_argument("--target", type=str, default=platform.system(),
-                        help="The build target, defaults to current os")
+  logging.basicConfig(format="%(message)s", level=logging.INFO)
+  parser = argparse.ArgumentParser(
+      description="Configures the android emulator cmake project so it can be build"
+  )
+  parser.add_argument(
+      "--out_dir", type=str, required=True, help="The ouput directory")
+  parser.add_argument(
+      "--dist_dir", type=str, required=True, help="The destination directory")
+  parser.add_argument(
+      "--build-id",
+      type=str,
+      default=[],
+      required=True,
+      dest="build_id",
+      help="The emulator build number")
+  parser.add_argument(
+      "--target",
+      type=str,
+      default=platform.system(),
+      help="The build target, defaults to current os")
 
-    args = parser.parse_args()
-    logging.info("Building on %s - %s", platform.system(), platform.uname())
+  args = parser.parse_args()
+  version = "{0[0]}.{0[1]}.{0[2]}".format(sys.version_info)
+  log_line("inf", "Building on {} - {}, Python: {}".format(platform.system(),
+                                                            platform.uname(), version))
 
-    ext = ''
-    if platform.system() == 'Windows':
-        ext = '.bat'
+  target = platform.system().lower()
+  if args.target == "Mingw":
+    target = "mingw"
 
+  if not os.path.isabs(args.out_dir):
+    args.out_dir = os.path.join(AOSP_ROOT, args.out_dir)
 
-    mingw = False
-    sdk = 'makeSdk'
-    if args.target == 'Windows':
-        sdk = 'makeWinSdk'
+  # Make sure we have all the build dependencies
+  install_deps()
 
-    if args.target == 'Mingw':
-        sdk = 'makeWinSdk'
-        mingw = True
-
-
-
-    if not os.path.isabs(args.out_dir):
-        args.out_dir = os.path.join(AOSP_ROOT, args.out_dir)
-
-    env = {
-        "OUT_DIR": args.out_dir,
-        "DIST_DIR": args.dist_dir,
-        "BUILD_NUMBER": args.build_id,
-        "MINGW" : "%s" % mingw
-    }
-
-    gradle = os.path.join(TOOLS, 'gradlew%s' % ext)
-    build_gradle = os.path.join(TOOLS, 'build.gradle')
-
-    # Make sure we have all the build dependencies
-    install_deps()
+  # This how we are going to launch the python build script
+  launcher = [
+      "python",
+      os.path.join(AOSP_ROOT, "external", "qemu", "android", "build", "python",
+                   "cmake.py")
+  ]
 
 
-    logging.info("Preparing gradle.")
-    run([gradle, '-b', build_gradle, '--no-daemon', '--info', 'publishLocal'], env)
+  # Standard arguments for both debug & production.
+  args = [
+      "--noqtwebengine", "--noshowprefixforinfo", "--out", args.out_dir,
+      "--sdk_build_number", args.build_id, "--target", target, "--dist",
+      args.dist_dir
+  ]
+  prod = ["--crash", "prod", "--symbols", "--symbol_dest", "prod"]
+  debug = ["--config", "debug"]
 
-    logging.info("Starting actual build.")
-    run([gradle, '-b', build_gradle, '--no-daemon', '--info', 'dist', sdk], env)
+
+  # Kick of builds for 2 targets. (debug/release)
+  run(launcher + args + prod, {}, "rel")
+  run(launcher + args + debug, {}, "dbg")
+  log_line("inf", "Build completed!")
 
 
-if __name__ == '__main__':
-    main(sys.argv)
+if __name__ == "__main__":
+  main(sys.argv)
