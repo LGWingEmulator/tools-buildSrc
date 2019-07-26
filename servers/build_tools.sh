67dcbd612f4233bff22d550edaa79ff2fb3ba558
Lingfeng Yang <lfy@google.com>
2019-07-26 08:42:00 -0700

    try to fix linux build bot
    probably some bash scripting bug somehwere
    
    Change-Id: I9fabc9c793c6074580dee9dfa3016abe2b449e2c

--- servers/build_tools.sh
+++ servers/build_tools.sh
@@ -10,37 +10,39 @@ set -e
 PROG_DIR=$(dirname "$0")
 CURRENT_OS=$(uname | tr A-Z a-z)
 
-function die() {
-  echo "$*" > /dev/stderr
-  echo "Usage: $0 <out_dir> <dest_dir> <build_number> [num_threads=47]" > /dev/stderr
-  exit 1
-}
+echo "$*" > /dev/stderr
 
-while [[ -n "$1" ]]; do
-  if [[ -z "$OUT_DIR" ]]; then
-    OUT_DIR="$1"
-  elif [[ -z "$DIST_DIR" ]]; then
-    DIST_DIR="$1"
-  elif [[ -z "$BNUM" ]]; then
-    BNUM="$1"
-  elif [[ -z "$NUM_THREADS" ]]; then
-    NUM_THREADS="$1"
-  else
-    die "[$0] Unknown parameter: $1"
-  fi
-  shift
-done
+# TODO: Figure out why this doesn't work
+# function die() {
+#   echo "Usage: $0 <out_dir> <dest_dir> <build_number> [num_threads=47]" > /dev/stderr
+#   exit 1
+# }
 
-if [[ -z "$OUT_DIR"  ]]; then die "## Error: Missing out folder"; fi
-if [[ -z "$DIST_DIR" ]]; then die "## Error: Missing destination folder"; fi
-if [[ -z "$BNUM"     ]]; then die "## Error: Missing build number"; fi
-
-if [[ "$OUT_DIR" != /* ]]
-then
-    pushd "$PROG_DIR"/../../..
-    OUT_DIR="$PWD/$OUT_DIR"
-    popd
-fi
+# while [[ -n "$1" ]]; do
+#   if [[ -z "$OUT_DIR" ]]; then
+#     OUT_DIR="$1"
+#   elif [[ -z "$DIST_DIR" ]]; then
+#     DIST_DIR="$1"
+#   elif [[ -z "$BNUM" ]]; then
+#     BNUM="$1"
+#   elif [[ -z "$NUM_THREADS" ]]; then
+#     NUM_THREADS="$1"
+#   else
+#     die "[$0] Unknown parameter: $1"
+#   fi
+#   shift
+# done
+# 
+# if [[ -z "$OUT_DIR"  ]]; then die "## Error: Missing out folder"; fi
+# if [[ -z "$DIST_DIR" ]]; then die "## Error: Missing destination folder"; fi
+# if [[ -z "$BNUM"     ]]; then die "## Error: Missing build number"; fi
+# 
+# if [[ "$OUT_DIR" != /* ]]
+# then
+#     pushd "$PROG_DIR"/../../..
+#     OUT_DIR="$PWD/$OUT_DIR"
+#     popd
+# fi
 
 # Of course we are running on build bots that have old.. old.. stuff on them.
 # So let's make sure we have enums available.
@@ -49,4 +51,4 @@ fi
   # pip install enum34 --user
 # fi
 
-python $PROG_DIR/build_tools.py --out_dir $OUT_DIR --dist_dir $DIST_DIR --build-id $BNUM
+python $PROG_DIR/build_tools.py --out_dir $1 --dist_dir $2 --build-id $3
